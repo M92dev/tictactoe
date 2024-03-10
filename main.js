01672cab746a9b4ce2c8ee999c336a43a7cae95b
M92 <m92dev@mail.de>
2024-03-10 21:01:34 +0100

    grundgerüst in html und css aufgebaut

---
+++ main.js
@@ -0,0 +1,13 @@
+/*
+
+1. HTMl 
+    - Seite erstellen 
+        - Auf der Seite gibt es ein Gitter mit den Feldern 3x3 (Grid wahrscheinlich)
+        - Es gibt den den Button "Spiel Starten"
+            - Nach dem Spiel starten gedrückt wurde, werden zwei Eingabfelder angezeit, in dem die Spielernamen eingegeben werden sollen. 
+            - Button mit "Los geht´s". -> Spiel Startet
+        - Es gibt einen Neustart button, der die Spielfläche leert 
+        - Es gibt einen nächste Runde Button
+            - diesen gibt es aber erst, sobald eine RUnde gespielt worden ist
+
+*/
